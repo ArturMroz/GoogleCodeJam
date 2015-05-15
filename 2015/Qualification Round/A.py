044d8a951350c94580496c82997f23bc7ac55d4b
ArturMroz <frost47@gmail.com>
2015-05-15 22:41:49 +0100

    + 2015

---
+++ 2015/Qualification Round/A.py
@@ -0,0 +1,15 @@
+# Standing Ovation
+# https://code.google.com/codejam/contest/6224486/dashboard#s=p0
+
+for tc in range(int(input())):
+    a = map(int, input().split()[1])
+    t = ans = 0
+    
+    for i, x in enumerate(a):
+        if x > 0 and i > t:
+            d = i - t
+            ans += d
+            t += d
+        t += x
+
+    print('Case #{}: {}'.format(tc + 1, ans))
